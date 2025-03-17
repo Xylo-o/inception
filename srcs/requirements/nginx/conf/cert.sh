b9d2db69460a236bbca4d7fd6c439e75e22dbf25
Xylo-o <tuadrian1327@gmail.com>
2025-03-17 10:13:10 +0000

    Created a script that creates a self-signed certificate + updated dockerfile and configuration file for nginx

---
+++ srcs/requirements/nginx/conf/cert.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This script generates a self-signed certificate for NGINX.
+# Usage: ./generate_cert.sh [DOMAIN_NAME]
+# Default DOMAIN_NAME is taken from the environment variable or set to "adprzyby.42.fr"
+
+DOMAIN_NAME=${1:-${DOMAIN_NAME:-"adprzyby.42.fr"}}
+CERT_DIR=$(dirname "$0")
+CRT_FILE="$CERT_DIR/certificate.crt"
+KEY_FILE="$CERT_DIR/cert_key.key"
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout "$KEY_FILE" \
+    -out "$CRT_FILE" \
+    -subj "/CN=$DOMAIN_NAME"
+
+echo "${\033[0;32m}Self-signed certificate generated:${\033[0m}"
+echo "${\033[0;34m}Certificate:${\033[0m} $CRT_FILE"
+echo "${\033[0;36m}Key:${\033[0m} $KEY_FILE"
