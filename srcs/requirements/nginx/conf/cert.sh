693cdb0bf7610f85e32254e249cb8abd70471325
Xyloo <adprzyby@student.42heilbronn.de>
2025-04-01 12:21:52 +0200

    added port exposure for wordpress in docker compose + updated cert generation and nginx config file

--- srcs/requirements/nginx/conf/cert.sh
+++ srcs/requirements/nginx/conf/cert.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-# This script generates a self-signed certificate for NGINX.
-
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/ssl/private/cert_key.key \
     -out /etc/ssl/certs/certificate.crt \
-    -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
+    -subj "/C=US/ST=State/L=City/O=Organization/CN=adprzyby.42.fr"
 
 chmod 644 /etc/ssl/certs/certificate.crt
 chmod 600 /etc/ssl/private/cert_key.key
